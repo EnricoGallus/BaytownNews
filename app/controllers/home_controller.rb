5f90421f37e1b18267904fe44e9b5e442d05ef0a
enrico <enrico.gallus@gmail.com>
2023-03-18 05:41:12 +0900

    add rubocop and fix all style issues

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# controller for our the index page
+# all user are allowed to request these
+class HomeController < ApplicationController
+  def index; end
+end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# default settings for the mailer
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
