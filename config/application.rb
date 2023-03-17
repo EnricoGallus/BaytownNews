a3d680b31af5abc2ff2d248487d4e390309d9a7f
enrico <enrico.gallus@gmail.com>
2023-03-18 05:30:49 +0900

    using bootstrap

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BaytownNews
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.application_name = Rails.application.class.module_parent_name
 
     # Configuration for the application, engines, and railties goes here.
     #
